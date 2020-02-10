ab890b6f9387540cb1be0f6c081f96daaa2e2654
Carter Carlson <carlfarterson@gmail.com>
2020-02-10 13:09:50 -0600

    Initial commit

---
+++ example/rebalancing-library.sol
@@ -0,0 +1,37 @@
+
+// File: contracts/core/lib/RebalancingLibrary.sol
+
+pragma solidity 0.5.7;
+
+
+/**
+ * @title RebalancingLibrary
+ * @author Set Protocol
+ *
+ * The RebalancingLibrary contains functions for facilitating the rebalancing process for
+ * Rebalancing Set Tokens. Removes the old calculation functions
+ *
+ */
+library RebalancingLibrary {
+
+    /* ============ Enums ============ */
+
+    enum State { Default, Proposal, Rebalance, Drawdown }
+
+    /* ============ Structs ============ */
+
+    struct AuctionPriceParameters {
+        uint256 auctionStartTime;
+        uint256 auctionTimeToPivot;
+        uint256 auctionStartPrice;
+        uint256 auctionPivotPrice;
+    }
+
+    struct BiddingParameters {
+        uint256 minimumBid;
+        uint256 remainingCurrentSets;
+        uint256[] combinedCurrentUnits;
+        uint256[] combinedNextSetUnits;
+        address[] combinedTokenArray;
+    }
+}
