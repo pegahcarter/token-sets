214040d7e82f43e145b50b84f36bd4b68a18c817
Carter Carlson <carlfarterson@gmail.com>
2020-03-18 17:55:03 -0500

    Take a pause with testing after weird MACD results from BTC

--- FART Set/test.py
+++ FART Set/test.py
@@ -2,10 +2,10 @@
 import pandas as pd
 import numpy as np
 
+from TAcharts.indicators import tsi, rsi, macd, roc
 
 from is_rebalance import is_rebalance
-from TAcharts.indicators import tsi
-
+from portfolio import Portfolio
 
 
 coins = ['ETH', 'DAI']
@@ -13,16 +13,32 @@ coins = ['ETH', 'DAI']
 p = Portfolio(coins)
 p.start_units
 
-# Load prices for our own testing
-prices = pd.read_csv('../data/combined.csv')
-
-rebalance = is_rebalance(prices['date'], weekday='Saturday', hour=10)
 
 # Indices of rebalancing - used first index to test
+rebalance = is_rebalance(prices['date'], weekday='Saturday', hour=10)
 rebalance_indices = np.where(rebalance)[0]
 rebalance_index = rebalance_indices[0]
 
-slow, fast = 25, 13
-prices['TSI'] = tsi(prices['ETH'], slow=slow, fast=fast)
+
+# Load prices for our own testing
+prices = pd.read_csv('../data/combined.csv')
+
+slow, fast = 100, 52
+n = 14
+
+# prices['TSI_ETH'] = tsi(prices['ETH'], slow=slow, fast=fast)
+# prices['TSI_BTC'] = tsi(prices['BTC'], slow=slow, fast=fast)
+# prices['RSI_ETH'] = rsi(prices['ETH'], n=n)
+# prices['RSI_BTC'] = rsi(prices['BTC'], n=n)
+prices['MACD_ETH'] = macd(prices['ETH'], slow=slow, fast=fast)
+prices['MACD_BTC'] = macd(prices['BTC'], slow=slow, fast=fast)
+prices['MACD_ROC_ETH'] = roc(prices['MACD_ETH'], n=4)
+prices['MACD_ROC_BTC'] = roc(prices['MACD_BTC'], n=4)
+
 
 prices.iloc[rebalance_indices][:10]
+
+prices['MACD_ETH'].describe()
+prices['MACD_BTC'].describe()
+
+prices['MACD_BTC'].sort_values()[:40]
