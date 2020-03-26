419db652de800bd4686708d45150a412419043be
Carter Carlson <carlfarterson@gmail.com>
2020-03-26 16:42:02 -0500

    Figure out bear/bull indices with apply_across

--- FART Set/test.py
+++ FART Set/test.py
@@ -2,43 +2,57 @@
 import pandas as pd
 import numpy as np
 
-from TAcharts.indicators import tsi, rsi, macd, roc
+from TAcharts.indicators import sma, rolling, rsi
+from TAcharts.utils import apply_across
+from TAcharts.wrappers import pd_series_to_np_array
 
+from signals import signals
 from is_rebalance import is_rebalance
-from portfolio import Portfolio
 
 
-coins = ['ETH', 'DAI']
-
-p = Portfolio(coins)
-p.start_units
-
+prices = pd.read_csv('../data/ETH-USDT.csv', usecols=['date', 'close']).rename({'close': 'ETH'}, axis=1)
 
 # Indices of rebalancing - used first index to test
 rebalance = is_rebalance(prices['date'], weekday='Saturday', hour=10)
-rebalance_indices = np.where(rebalance)[0]
-rebalance_index = rebalance_indices[0]
+rebalance_indices = np.flatnonzero(rebalance)
 
 
-# Load prices for our own testing
-prices = pd.read_csv('../data/combined.csv')
+ma_50 = sma(prices['ETH'], n=50)
+ma_100 = sma(prices['ETH'], n=100)
+ma_200 = sma(prices['ETH'], n=200)
+
+bullish = (ma_50 > ma_100) & (ma_100 > ma_200)
+# bullish_2 = rsi(prices['ETH'], n=14) > 50
+bearish = (ma_50 < ma_100) & (ma_100 < ma_200)
+
+# bull_trades = signals(rebalance, bullish)
+# bear_trades = signals(rebalance, bearish)
+
+# Bullish signals
+%timeit bull_indices = np.flatnonzero(apply_across(rebalance, bullish, fn='min'))
+
+bear_indices = np.flatnonzero(apply_across(rebalance, bearish, fn='min'))
+
+
+
+
+
+
+len(bullish)
+
+
+
+
+
+
 
 slow, fast = 100, 52
 n = 14
-
 # prices['TSI_ETH'] = tsi(prices['ETH'], slow=slow, fast=fast)
 # prices['TSI_BTC'] = tsi(prices['BTC'], slow=slow, fast=fast)
 # prices['RSI_ETH'] = rsi(prices['ETH'], n=n)
 # prices['RSI_BTC'] = rsi(prices['BTC'], n=n)
-prices['MACD_ETH'] = macd(prices['ETH'], slow=slow, fast=fast)
-prices['MACD_BTC'] = macd(prices['BTC'], slow=slow, fast=fast)
-prices['MACD_ROC_ETH'] = roc(prices['MACD_ETH'], n=4)
-prices['MACD_ROC_BTC'] = roc(prices['MACD_BTC'], n=4)
-
-
-prices.iloc[rebalance_indices][:10]
-
-prices['MACD_ETH'].describe()
-prices['MACD_BTC'].describe()
-
-prices['MACD_BTC'].sort_values()[:40]
+# prices['MACD_ETH'] = macd(prices['ETH'], slow=slow, fast=fast)
+# prices['MACD_BTC'] = macd(prices['BTC'], slow=slow, fast=fast)
+# prices['MACD_ROC_ETH'] = roc(prices['MACD_ETH'], n=4)
+# prices['MACD_ROC_BTC'] = roc(prices['MACD_BTC'], n=4)
