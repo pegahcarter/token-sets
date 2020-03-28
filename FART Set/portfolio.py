fc6b27be26079ea549e4b1b9bdfd5b141137c70d
Carter Carlson <carlfarterson@gmail.com>
2020-03-28 02:27:00 -0500

    Save ETH-USDT data from binance and rename combined.csv > prices.csv

--- FART Set/portfolio.py
+++ FART Set/portfolio.py
@@ -11,7 +11,7 @@ class Portfolio:
     def __init__(self, coins):
 
         self.coins = coins
-        hist_prices = pd.read_csv('../data/combined.csv', usecols=['date'] + coins)
+        hist_prices = pd.read_csv('../data/prices.csv', usecols=['date'] + coins)
         self.dates = hist_prices.pop('date')
         self.hist_prices = hist_prices.values
         prices = self.hist_prices[0]
@@ -21,3 +21,5 @@ class Portfolio:
         self.start_prices = prices
         self.start_units = units.copy()
         self.units = units
+
+        self.trade_count = 0
