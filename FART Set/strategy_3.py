49b23fdec60fec6343da1d5001743c847424d0c7
Carter Carlson <carlfarterson@gmail.com>
2020-03-28 23:21:25 -0500

    Create three template options for creating backtest windows

--- FART Set/strategy_3.py
+++ FART Set/strategy_3.py
@@ -9,3 +9,38 @@ from is_rebalance import is_rebalance
 from portfolio import Portfolio
 from signals import signals
 from variables import *
+
+
+# Create rolling window from a list
+test = list(range(11))
+
+# -----------------------------------
+# Option 1
+# NOTE: best option
+overlap = 3
+window_len = 5
+
+for i in range(0, len(test) - window_len + 1, overlap + 1):
+    print(test[i:i+window_len])
+
+# -----------------------------------
+# Option 2
+overlap = 3
+num_windows = 2
+
+window_len = int((len(test) + overlap * (num_windows - 1)) / num_windows)
+
+for i in range(0, len(test) - window_len + 1, overlap + 1):
+    print(test[i:i+window_len])
+
+
+# -----------------------------------
+# Option 3
+# NOTE: windows might not overlap
+window_len = 5
+num_windows = 3
+
+overlap = int((window_len * num_windows - len(test)) / (num_windows - 1))
+
+for i in range(0, len(test) - window_len + 1, overlap + 1):
+    print(test[i:i+window_len])
