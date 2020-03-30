686a655a83719fd9097791982773298053f9dcc7
Carter Carlson <carlfarterson@gmail.com>
2020-03-30 12:58:24 -0500

    Add basic test split of prices dataframe

--- FART Set/strategy_3.py
+++ FART Set/strategy_3.py
@@ -4,16 +4,47 @@ import pandas as pd
 import numpy as np
 
 from TAcharts.indicators import sma
+from datetime import datetime, timedelta
 
 from is_rebalance import is_rebalance
 from portfolio import Portfolio
 from signals import signals
 from variables import *
 
+prices = pd.read_csv('../data/prices.csv')
+prices['date'] = pd.to_datetime(prices['date'])
 
+# First function to wrap option 1
+
+def foo(array, overlap, window_len):
+
+    array_split = []
+
+    for x in range(0, len(array) - window_len + 1, overlap - 1):
+        array_split.append(array[x:x+window_len])
+
+    return array_split
+
+
+# Figuring out how to split up prices dataset
+day_diff = prices['date'].iat[-1] - prices['date'].iat[0]
+
+# Let's say we want 100 day windows with 50 day overlap
+# Well, since we're using hours, let's convert into days and go that way
+window_len = 24 * 100
+overlap = 24 * 50
+
+test = foo(prices, overlap, window_len)
+len(test)
+
+
+
+
+
+# ------------------------------------------------------------------------------
 # Create rolling window from a list
-test = list(range(11))
 
+test = list(range(11))
 # -----------------------------------
 # Option 1
 # NOTE: best option
@@ -44,16 +75,3 @@ overlap = (window_len * num_windows - len(test)) // (num_windows - 1)
 
 for i in range(0, len(test) - window_len, overlap - 1):
     print(test[i:i+window_len])
-
-# ------------------------------------------------------------------------------
-# First function to wrap option 1
-
-def foo(array, overlap, window_len):
-
-    for x in range(0, len(array) - window_len + 1, overlap - 1):
-        print(array[x:x+window_len])
-
-
-df = prices[:10]
-
-foo(df, overlap=3, window_len=5)
