ffa5393c4194ce0530f400f0df4d05add2054703
Carter Carlson <carlfarterson@gmail.com>
2020-04-02 23:41:30 -0500

    Simplify portfolio class

--- FART Set/portfolio.py
+++ FART Set/portfolio.py
@@ -6,20 +6,15 @@ import numpy as np
 class Portfolio:
 
     INITIAL_CAPITAL = 10000
-    SLIPPAGE = 0.008
+    SLIPPAGE = 0.006
 
-    def __init__(self, coins):
+    def __init__(self, coins, start_prices):
 
-        self.coins = coins
-        hist_prices = pd.read_csv('../data/prices.csv', usecols=['date'] + coins)
-        self.dates = hist_prices.pop('date')
-        self.hist_prices = hist_prices.values
-        prices = self.hist_prices[0]
         amt_each = self.INITIAL_CAPITAL / len(coins)
-        units = np.divide(amt_each, prices)
+        units = np.divide(amt_each, start_prices)
 
-        self.start_prices = prices
-        self.start_units = units.copy()
+        self.start_prices = start_prices
         self.units = units
+        self.start_units = units.copy()
 
         self.trade_count = 0
