60c937c48b8f568a898e95dc1812ef61cf426542
Carter Carlson <carlfarterson@gmail.com>
2020-04-05 03:42:28 -0500

    Add documentation

--- FART Set/is_rebalance.py
+++ FART Set/is_rebalance.py
@@ -1,10 +1,11 @@
-# Add boolean value to rebalance based on weekday and hour
+# Add boolean value to rebalance based on day and hour
 import pandas as pd
 
 
-def is_rebalance(dates, weekday='Saturday', hour=10):
+# NOTE: default day and hour determined from `Determine Rebalance Day and Hour.ipynb`
+def is_rebalance(dates, day='Saturday', hour=10):
 
-    is_weekday = pd.DatetimeIndex(dates).day_name() == weekday
+    is_day = pd.DatetimeIndex(dates).day_name() == day
     is_hour = pd.to_datetime(dates).apply(lambda x: x.hour == hour and x.minute == 0)
 
-    return is_weekday & is_hour
+    return is_day & is_hour
