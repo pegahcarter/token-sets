194dcb1de6528534050b404f68b8378171c5dbff
Carter Carlson <carlfarterson@gmail.com>
2020-04-05 03:49:46 -0500

    Create main.py

---
+++ FART Set/main.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+from is_rebalance import is_rebalance
+from split_df import split_df
+from signals import signals
+from variables import *
+
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
