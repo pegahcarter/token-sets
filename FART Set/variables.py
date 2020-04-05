8dec1465d2cc04e40a8b5248493288d2d368fadd
Carter Carlson <carlfarterson@gmail.com>
2020-04-05 02:12:02 -0500

    Add comments to variables

--- FART Set/variables.py
+++ FART Set/variables.py
@@ -2,9 +2,13 @@
 import pandas as pd
 import numpy as np
 
+# Coins traded
 coins = ['ETH', 'DAI']
 
+# Moving average intervals used
+moving_averages = [50, 100, 200]
 
+# Potential ETH to DAI allocations from bullish signals
 bull_allocation = [
     [0.90, 0.10],
     [0.85, 0.15],
@@ -15,19 +19,13 @@ bull_allocation = [
     [0.60, 0.40]
 ]
 
+# List of allocations used, with the inverse allocation for bearish signals
 allocation_lst = [{'bull': b,
                    'neutral': [0.50, 0.50],
                    'bear': b[::-1]}
-                  for b in bull_allocations]
+                  for b in bull_allocation]
 
 
-
-allocation = {
-    'bull': [0.75, 0.25],
-    'neutral': [0.50, 0.50],
-    'bear': [0.25, 0.75]
-}
-
-wiggle_room_lst = np.arange(0, 0.17, 0.02)
-
-moving_averages = [50, 100, 200]
+# Minimum difference in weighting needed to rebalance without a new signal
+# This prevents unnecessary rebalancing
+wiggle_room_lst = np.arange(0, 0.16, 0.01)
