7c6dff86470e8c22070ecb88150771b42a647a9f
Carter Carlson <carlfarterson@gmail.com>
2020-04-13 21:47:15 -0500

    Issued a Set on Kovan

--- testing/kovan-tokenset/1. Create set.js
+++ testing/kovan-tokenset/1. Create set.js
@@ -23,7 +23,7 @@ const INFURA_URL_HTTPS = env['INFURA']['kovan']['HTTPS'] + env['INFURA']['ID']
 const INFURA_URL_WS = env['INFURA']['kovan']['WS'] + env['INFURA']['ID']
 const KOVAN_CONFIG = env['configSetProtocol']['kovan']['config']
 
-let provider = new Web3(new HDWalletProvider(PRIVATE_KEY, INFURA_URL_HTTPS));
+let web3 = new Web3(new HDWalletProvider(PRIVATE_KEY, INFURA_URL_HTTPS));
 
 // let provider = new ethers.providers.JsonRpcProvider(INFURA_URL_HTTPS);
 // let wallet = new ethers.Wallet(PRIVATE_KEY, provider);
@@ -32,7 +32,7 @@ let provider = new Web3(new HDWalletProvider(PRIVATE_KEY, INFURA_URL_HTTPS));
 // provider.eth.accounts.wallet.create();
 // provider.eth.accounts.wallet.add(PRIVATE_KEY);
 
-const setProtocol = new SetProtocol(provider, KOVAN_CONFIG)
+const setProtocol = new SetProtocol(web3, KOVAN_CONFIG)
 // const setProtocol = new SetProtocol(wallet, KOVAN_CONFIG)
 
 const addressTrueUSD = '0xadb015d61f4beb2a712d237d9d4c5b75bafefd7b';
@@ -47,13 +47,12 @@ const txOpts = {
 };
 
 
-// VERSION 1
-async function createSet() {
+const createSet = async function() {
   const { units, naturalUnit } = await setProtocol.calculateSetUnitsAsync(
     componentAddresses,
     [new BigNumber(1), new BigNumber(1)],
     [new BigNumber(0.75), new BigNumber(0.25)],
-    new BigNumber(100),  // $100 start token price
+    new BigNumber(50),  // $50 start token price
   );
   const txHash = await setProtocol.createSetAsync(
     componentAddresses,
@@ -63,8 +62,47 @@ async function createSet() {
     symbol,
     txOpts,
   );
-  console.log(txHash);
+  // const receipt = web3.eth.getTransactionReceipt(txHash).then(console.log);
   return await setProtocol.getSetAddressFromCreateTxHashAsync(txHash);
 };
 
-const setAddress = createSet();
+
+const approveTokensForTransfer = (tokenAddresses) => {
+  tokenAddresses.forEach(async function(address) {
+    await setProtocol.setUnlimitedTransferProxyAllowanceAsync(
+      address, { gas: 4000000, gasPrice: 6000000000 },
+    );
+  });
+};
+
+
+const issueSet = async function(setAddress) {
+  // Issue 1x Set which equals 50 * 10 ** 18 base units.
+  const issueQuantity = new BigNumber(10 ** 18);
+
+  // Check that our issue quantity is divisible by the natural unit.
+  const isMultipleOfNaturalUnit = await setProtocol.setToken.isMultipleOfNaturalUnitAsync(setAddress, issueQuantity);
+
+  if (isMultipleOfNaturalUnit) {
+    try {
+      return await setProtocol.issueAsync(
+        setAddress,
+        issueQuantity,
+        txOpts,
+      );
+    } catch (err) {
+      throw new Error(`Error when issuing a new Set token: ${err}`);
+    }
+  }
+  throw new Error(`Issue quantity is not multiple of natural unit. Confirm that your issue quantity is divisible by the natural unit.`);
+};
+
+
+
+createSet().then((setAddress) => {
+  console.log(setAddress); // 0x869b8ad1ca9fb33ae0f8f9095574b1439a984891
+  // Approve all component tokens for transfer to Set Protocol contracts
+  approveTokensForTransfer(componentAddresses);
+  // Issue one unit of our Set
+  issueSet(setAddress).then(console.log);  /// 0x67af8476c231d30a80e200a48e9138703477581555cc0edc1719eae96cc4e30e
+});
