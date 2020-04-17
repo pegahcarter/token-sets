3cad8a8e8f91f56f2d4f38a651a4bc264fd3f4ff
Carter Carlson <carlfarterson@gmail.com>
2020-04-17 11:50:44 -0500

    Finish hacky way to calculate allocation

--- testing/token-set/3. Check set allocation.js
+++ testing/token-set/3. Check set allocation.js
@@ -23,33 +23,21 @@ const CoinGeckoClient = new CoinGecko();
 ;(async () => {
   // NOTE: this address from the getComponents() function of the Set contract (0xF9958A0D54721d48D75D9fD65Af29a1Ed406Ca4D)
   let setDetails = await setProtocol.setToken.getDetailsAsync('0xF9958A0D54721d48D75D9fD65Af29a1Ed406Ca4D');
-  console.log(setDetails);
 
   let componentAddresses = setDetails['components'].map(x => x['address'])
   let componentUnits = setDetails['components'].map(x => x['unit'].toNumber());
-  let baseUnit = setDetails.naturalUnit.toNumber();
-  // console.log(componentAddresses);
-  // console.log(componentUnits);
-  // console.log(baseUnit);
-  // componentUnits.forEach(x => console.log(x / baseUnit));
 
-  // let componentPricesResponse = await CoinGeckoClient.simple.fetchTokenPrice({
-  //   contract_addresses: componentAddresses,
-  //   vs_currencies: 'usd'
-  // });
-  // let componentPrices = Object.values(componentPricesResponse['data']).map(x => x['usd']);
-  // console.log(componentPrices)
+  let componentPricesResponse = await CoinGeckoClient.simple.fetchTokenPrice({
+    contract_addresses: componentAddresses,
+    vs_currencies: 'usd'
+  });
+  let componentPrices = Object.values(componentPricesResponse['data']).map(x => x['usd']);
 
-  // const componentValues = mathjs.multiply(componentUnits, componentPrices)
-  // console.log(componentValues);
+  // Update USDC with units
+  let componentValues = [componentPrices[0], componentPrices[1] * componentUnits[1]/1000];
 
-  // let units2 = await setProtocol.setToken.getUnitsAsync('0xffee21b4bb7084a9416205544101ae9f472c7159');
-  // units2.forEach(x => console.log(x.toNumber()));
-
-
-
-
-
+  let [ethWeight, cdaiWeight] = mathjs.divide(componentValues, mathjs.sum(componentValues))
+  console.log(ethWeight);
 
   // NOTE: this dispalys the same WETH that is shown in underlying tokens
   // let setDetails = await setProtocol.setToken.getDetailsAsync('0xffee21b4bb7084a9416205544101ae9f472c7159');
