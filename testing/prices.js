5f34bb32cfdc14f01891e087d6e645474f228108
Carter Carlson <carlfarterson@gmail.com>
2020-04-17 13:52:49 -0500

    Add conditional for signal

--- testing/prices.js
+++ testing/prices.js
@@ -12,8 +12,16 @@ const sma = require('sma');
   let sma100 = sma(closePrices, 100)
   let sma200 = sma(closePrices, 200)
 
-  console.log(parseFloat(sma50[sma50.length - 1]));
-  console.log(parseFloat(sma100[sma100.length - 1]));
-  console.log(parseFloat(sma200[sma200.length - 1]));
+  let current50 = parseFloat(sma50[sma50.length - 1])
+  let current100 = parseFloat(sma100[sma100.length - 1])
+  let current200 = parseFloat(sma200[sma200.length - 1])
+
+  if (current50 > current100 && current100 > current200) {
+    signal = 'bull'
+  } else if (current50 < current100 && current100 < current200) {
+    signal = 'bear'
+  } else {
+    signal = 'neutral'
+  }
 
 })();
