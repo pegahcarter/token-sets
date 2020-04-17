3073e5a5451ebcce77865af40ab38bccc30c5433
Carter Carlson <carlfarterson@gmail.com>
2020-04-17 11:11:26 -0500

    Finished returning component prices... Still need to figure out allocations

--- testing/token-set/3. Check set allocation.js
+++ testing/token-set/3. Check set allocation.js
@@ -10,7 +10,7 @@ const mathjs = require('mathjs');
 const file = fs.readFileSync('../config.yaml', 'utf8')
 const env = YAML.parse(file)
 
-const PUBLIC_KEY = env['addresses']['demo']['PUBLIC_KEY']
+const PUBLIC_KEY = env['addresses']['mainnet']['PUBLIC_KEY']
 const PRIVATE_KEY = env['addresses']['demo']['PRIVATE_KEY']
 const INFURA_URL_HTTPS = env['INFURA']['mainnet']['HTTPS'] + env['INFURA']['ID']
 const MAINNET_CONFIG = env['configSetProtocol']['mainnet']
@@ -21,29 +21,40 @@ const setProtocol = new SetProtocol(web3, MAINNET_CONFIG)
 const CoinGeckoClient = new CoinGecko();
 
 ;(async () => {
-  // NOTE: this address from the getComponents() function of the Set contract (0xffee21b4bb7084a9416205544101ae9f472c7159)
-  let setDetails = await setProtocol.setToken.getDetailsAsync('0xf9958a0d54721d48d75d9fd65af29a1ed406ca4d');
-
+  // NOTE: this address from the getComponents() function of the Set contract (0xF9958A0D54721d48D75D9fD65Af29a1Ed406Ca4D)
+  let setDetails = await setProtocol.setToken.getDetailsAsync('0xF9958A0D54721d48D75D9fD65Af29a1Ed406Ca4D');
   console.log(setDetails);
 
   let componentAddresses = setDetails['components'].map(x => x['address'])
   let componentUnits = setDetails['components'].map(x => x['unit'].toNumber());
+  let baseUnit = setDetails.naturalUnit.toNumber();
+  // console.log(componentAddresses);
+  // console.log(componentUnits);
+  // console.log(baseUnit);
+  // componentUnits.forEach(x => console.log(x / baseUnit));
 
   // let componentPricesResponse = await CoinGeckoClient.simple.fetchTokenPrice({
   //   contract_addresses: componentAddresses,
   //   vs_currencies: 'usd'
   // });
-  //
   // let componentPrices = Object.values(componentPricesResponse['data']).map(x => x['usd']);
-  //
-  // console.log(componentAddresses);
-  // console.log(componentUnits);
   // console.log(componentPrices)
-
-  let test = await setProtocol.setToken.getUnitsAsync('0xf9958a0d54721d48d75d9fd65af29a1ed406ca4d');
-  console.log(test);
 
   // const componentValues = mathjs.multiply(componentUnits, componentPrices)
   // console.log(componentValues);
+
+  // let units2 = await setProtocol.setToken.getUnitsAsync('0xffee21b4bb7084a9416205544101ae9f472c7159');
+  // units2.forEach(x => console.log(x.toNumber()));
+
+
+
+
+
+
+  // NOTE: this dispalys the same WETH that is shown in underlying tokens
+  // let setDetails = await setProtocol.setToken.getDetailsAsync('0xffee21b4bb7084a9416205544101ae9f472c7159');
+  // let ethUnits = setDetails.components[0].unit.toNumber();
+  // let baseUnit = setDetails.naturalUnit.toNumber();
+  // console.log(ethUnits / baseUnit);
 
 })()
