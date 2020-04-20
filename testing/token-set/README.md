2441f0eb0ee2f529cbf48d027e4b4f9c5c2f3a7f
Carter Carlson <carlfarterson@gmail.com>
2020-04-20 11:13:35 -0500

    Move javascript testing to testing directory

--- testing/token-set/README.md
+++
@@ -1,4 +0,0 @@
-## kovan
-
-### Description
-This directory is used for creating, issuing, and rebalancing a token set on Ethereum testnet (Kovan).
--- testing/uilts.js
+++
@@ -1,6 +0,0 @@
-
-const formatAllocation = (allocation) => {
-  allocationBigNumber = new BigNumber(allocation)
-  const allocationInWei = web3.utils.toWei(allocationBigNumber.toString(), 'ether');
-  return new BigNumber(allocationInWei);
-};
