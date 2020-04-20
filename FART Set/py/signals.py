632cce2feed089cadc00857d71976a540f6b3e79
Carter Carlson <carlfarterson@gmail.com>
2020-04-19 21:20:07 -0500

    Remove __name__ == '__main__' code

--- FART Set/py/signals.py
+++ FART Set/py/signals.py
@@ -6,8 +6,6 @@ import itertools
 from TAcharts.indicators import sma
 from TAcharts.utils import apply_across
 
-from is_rebalance import is_rebalance
-
 
 def signals(src, rebalance, n_fast, n_mid, n_slow):
 
@@ -28,15 +26,3 @@ def signals(src, rebalance, n_fast, n_mid, n_slow):
     _signals.update(zip(bear_indices, itertools.repeat('bear')))
 
     return pd.Series(_signals)
-
-
-
-if __name__ == '__main__':
-
-    df = pd.read_csv('../../data/ETH-USDT.csv', usecols=['date', 'open']).rename({'open': 'ETH'}, axis=1)
-    df['USD'] = 1
-    df['rebalance_daily'] = is_rebalance(df['date'], day=None, hour=10)
-    df['rebalance_weekly'] = is_rebalance(df['date'], day='Saturday', hour=10)
-
-    df['signal'] = signals(df['ETH'], df['rebalance_daily'], 50, 100, 200)
-    df.to_csv('../backtests/signals.csv', index=False)
